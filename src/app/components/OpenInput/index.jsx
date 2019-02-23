e55339e2fabb559ea2d0eca8e956a02afe2ead9a
David Haeffner <dhaeffner@gmail.com>
2019-02-23 10:30:29 +0200

    Added filename persistence in the popup view

--- src/app/components/OpenInput/index.jsx
+++ src/app/components/OpenInput/index.jsx
@@ -1,3 +1,4 @@
+import browser from 'webextension-polyfill'
 import React from 'react'
 import PropTypes from 'prop-types'
 import './style.css'
@@ -9,15 +10,33 @@ export default class OpenInput extends React.Component {
   }
   constructor(props) {
     super(props)
+    this.state = { uploadedFile: '' }
+    browser.storage.local.get(['uploadedFile']).then(({ uploadedFile }) => {
+      if (uploadedFile) this.setState({ uploadedFile })
+    })
     this.handleChange = this.handleChange.bind(this)
   }
   handleChange(event) {
-    this.props.onFileSelected(event.target.files[0])
+    const file = event.target.files[0]
+    browser.storage.local.set({ ['uploadedFile']: file.name })
+    this.setState({ uploadedFile: file.name })
+    this.props.onFileSelected(file)
   }
   render() {
     return (
       <div className="file-input">
+        <h4>Selenium IDE File Uploader</h4>
         <input type="file" onChange={this.handleChange} />
+        <br />
+        <br />
+        {this.state.uploadedFile ? (
+          <div>
+            <p>Currently Uploaded File:</p>
+            <p>{this.state.uploadedFile}</p>
+          </div>
+        ) : (
+          undefined
+        )}
       </div>
     )
   }
