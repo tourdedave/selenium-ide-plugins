6f9dfd5638397bc41d512f1db9c2826c95417beb
David Haeffner <dhaeffner@gmail.com>
2019-05-23 17:22:56 +0200

    Updated the CSV parser to a format that will work with the forEach command in Selenium IDE

--- file-uploader/src/background/__test__/parsers/index.spec.js
+++ file-uploader/src/background/__test__/parsers/index.spec.js
@@ -2,19 +2,17 @@ import { CSVtoJSON } from '../../parsers'
 
 describe('parsers', () => {
   describe('csv', () => {
+    const output = '[{"a":"d","b":"e","c":"f"},{"a":"g","b":"h","c":"i"}]'
     it('to json', () => {
       const input = 'a,b,c\nd,e,f\ng,h,i'
-      const output = '{"a":["d","g"],"b":["e","h"],"c":["f","i"]}'
       expect(CSVtoJSON(input)).toEqual(output)
     })
-    it('cleans empty array entries', () => {
+    it('ignores terminating empty line', () => {
       const input = 'a,b,c\nd,e,f\ng,h,i\n'
-      const output = '{"a":["d","g"],"b":["e","h"],"c":["f","i"]}'
       expect(CSVtoJSON(input)).toEqual(output)
     })
     it('handles windows newlines', () => {
       const input = 'a,b,c\r\nd,e,f\r\ng,h,i'
-      const output = '{"a":["d","g"],"b":["e","h"],"c":["f","i"]}'
       expect(CSVtoJSON(input)).toEqual(output)
     })
   })
--- file-uploader/src/background/parsers/index.js
+++ file-uploader/src/background/parsers/index.js
@@ -1,17 +1,17 @@
 export function CSVtoJSON(contents) {
-  let result = {}
-  const _contents = contents.split(/\r?\n/)
-  const keys = _contents.shift().split(',')
+  let result = []
+  const csv_rows = contents.split(/\r?\n/)
+  const csv_column_headings = csv_rows.shift().split(',')
 
-  keys.forEach(function(key, keyIndex) {
-    if (!result[key]) result[key] = []
-    _contents.forEach(function(contentRow) {
-      contentRow.split(',').forEach(function(content, contentIndex) {
-        if (keyIndex === contentIndex && content) {
-          result[key].push(content)
-        }
+  csv_rows.forEach(row => {
+    let row_result = {}
+    row = row.split(',')
+    if (row.length === csv_column_headings.length) {
+      row.forEach((entry, index) => {
+        row_result[csv_column_headings[index]] = entry
       })
-    })
+      result.push(row_result)
+    }
   })
 
   return JSON.stringify(result)
